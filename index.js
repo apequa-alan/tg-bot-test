de7da3905606fed9d2c0591f359cecdaec817cfa
Apequa <apequa.alan@gmail.com>
2025-01-01 15:58:21 +0300

    Fix code 4

--- index.js
+++ index.js
@@ -54,19 +54,19 @@ app.post('/web-data', async (req, res) => {
     console.log(req.body)
     console.log(Object.create(req.body))
     const {queryId, products = [], totalPrice} = req.body;
-    // try {
-    //     await bot.answerWebAppQuery(queryId, {
-    //         type: 'article',
-    //         id: queryId,
-    //         title: 'Успешная покупка',
-    //         input_message_content: {
-    //             message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-    //         }
-    //     })
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
         return res.status(200).json({queryId, products, totalPrice});
-    // } catch (e) {
-    //     return res.status(500).json({})
-    // }
+    } catch (e) {
+        return res.status(500).json({})
+    }
 })
 
 const PORT = 8000;
