c690cc0e9e9075800685f6ebea67f8972a456a8b
Apequa <apequa.alan@gmail.com>
2025-01-01 22:15:52 +0300

    Fix code 8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const token = '7718761845:AAFx6eWWCgeNfAC6FoxtRLkvl3yx6IUrM2w'
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true,  });
 const webAppUrl = 'https://tgminiappstoreapp.web.app'
 const app = express();
 
@@ -55,14 +55,14 @@ app.post('/web-data', async (req, res) => {
     console.log(req.body)
     const {queryId, products = [], totalPrice} = req.body;
     try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-            }
-        })
+        // await bot.answerWebAppQuery(queryId, {
+        //     type: 'article',
+        //     id: queryId,
+        //     title: 'Успешная покупка',
+        //     input_message_content: {
+        //         message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+        //     }
+        // })
         console.log('success')
         return res.status(200).json({queryId, products, totalPrice});
     } catch (error) {
