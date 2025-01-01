106b8d0159a6d484b43ac516f8e11bd2a452a0a6
Apequa <apequa.alan@gmail.com>
2025-01-01 15:56:38 +0300

    Fix code 3

--- index.js
+++ index.js
@@ -51,20 +51,22 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
+    console.log(req.body)
+    console.log(Object.create(req.body))
     const {queryId, products = [], totalPrice} = req.body;
-    try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-            }
-        })
-        return res.status(200).json({});
-    } catch (e) {
-        return res.status(500).json({})
-    }
+    // try {
+    //     await bot.answerWebAppQuery(queryId, {
+    //         type: 'article',
+    //         id: queryId,
+    //         title: 'Успешная покупка',
+    //         input_message_content: {
+    //             message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+    //         }
+    //     })
+        return res.status(200).json({queryId, products, totalPrice});
+    // } catch (e) {
+    //     return res.status(500).json({})
+    // }
 })
 
 const PORT = 8000;
